8e503777be4378e7f0981fd7f379ba69bdc704a6
Elek Márton <elek@apache.org>
2020-05-09 15:42:51 +0200

    working full secure (TLS + Kerberos) Ozone cluster

--- examples/secure/ca/trust.sh
+++
@@ -1,5 +0,0 @@
-
-
-openssl pkcs12 -export -nokeys -in out/ca.crt -out truststore.keystore
-
-#keytool -import -file cert.pem -alias trust -keystore keystore
--- examples/secure/getkeystore.sh
+++ examples/secure/getkeystore.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 STATEDIR=".state/keystore"
 mkdir -p "$STATEDIR"
 CADIR=".state/ca"
@@ -7,10 +8,8 @@ DESCRIPTOR="$2"
 STATEFILE="$STATEDIR/$NAME"
 if [ ! -f "$STATEFILE" ]; then
    source "$DESCRIPTOR"
-   PRIMARY_DOMAIN=ozone-om-0
-   OTHER_DOMAINS=ozone-om-0.ozone.om
-   certstrap --depot-path "$CADIR" request-cert --passphrase "" --domain $PRIMARY_DOMAIN,$OTHER_DOMAINS &1>2
-   certstrap --depot-path "$CADIR" sign --CA ca $PRIMARY_DOMAIN &1>2
+   certstrap --depot-path "$CADIR" request-cert --passphrase "" --domain $PRIMARY_DOMAIN,$OTHER_DOMAINS 1>&2
+   certstrap --depot-path "$CADIR" sign --CA ca $PRIMARY_DOMAIN 1>&2
    openssl pkcs12 -export \
        -in $CADIR/$PRIMARY_DOMAIN.crt \
        -inkey $CADIR/$PRIMARY_DOMAIN.key \

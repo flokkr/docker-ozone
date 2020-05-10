177ecf33b23b9f4312fc307d70452e5f840c8f70
Elek Márton <elek@apache.org>
2020-05-10 20:06:53 +0200

    updated secure configuration

--- examples/secure/getkeytab.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-kubectl exec krb5-0 -- rm /tmp/keytab 1>&2 || true
-kubectl exec krb5-0 -- kadmin -kt /tmp/admin.keytab -p admin/admin -q "addprinc -randkey $1" 1>&2
-kubectl exec krb5-0 -- kadmin -kt /tmp/admin.keytab -p admin/admin -q "ktadd -k /tmp/keytab $1" 1>&2
-kubectl exec krb5-0 -- cat /tmp/keytab | base64
